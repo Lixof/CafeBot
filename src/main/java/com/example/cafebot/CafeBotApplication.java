2e16d56bd56effa5a105f8aff34a6692f6c1f216
Viacheslav Likhov <likhov.ve@svrauto.ru>
2022-05-07 17:09:13 +0300

    first commit

---
+++ src/main/java/com/example/cafebot/CafeBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cafebot/CafeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
